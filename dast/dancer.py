3bd98ac95228a0939ba1be2e7db7372f2c3f954a
Marc Gorzala <marc@becheftigt.de>
2020-02-23 00:18:46 +0100

    implemented more methods

--- dast/dancer.py
+++ dast/dancer.py
@@ -1,6 +1,29 @@
 import requests
+import json
 
 
 def get_api():
     r = requests.get("http://localhost:8080/api")
+    return r
+
+
+def get_dancers():
+    r = requests.get("http://localhost:8080/api/dancers")
+    return r
+
+
+def get_dancer(id):
+    r = requests.get("http://localhost:8080/api/dancers/{}".format(id))
+    return r
+
+
+def post_dancer(first_name, last_name, public_name, dance):
+    data = {
+        "firstName": first_name,
+        "lastName": last_name,
+        "publicName": public_name,
+        "dance": dance
+    }
+    json_string = json.dumps(data)
+    r = requests.post("http://localhost:8080/api/dancers", data=json_string)
     return r
