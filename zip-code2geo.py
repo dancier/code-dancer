d250473fc7147269e97d0829f30ad3e6d4d5d4a0
Marc Gorzala <marc@dancier.net>
2022-04-25 12:18:44 +0200

    more on zip generation

--- zip-code2geo.py
+++ zip-code2geo.py
@@ -1,5 +1,7 @@
 import csv
 
+# download this file from: https://public.opendatasoft.com/explore/dataset/georef-germany-postleitzahl/table/
+
 file_name = "georef-germany-postleitzahl.csv"
 
 print(file_name)
@@ -8,12 +10,16 @@ with open(file_name, mode='r') as source_file:
     csv_reader = csv.DictReader(source_file, delimiter=';')
     line_count = 0
     for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {",".join(row.keys())}')
-            line_count += 1
         zip_code = row['Name']
         zip_kreis_name = row['Kreis name']
         zip_name_short = row['PLZ Name (short)']
-        zip_area_points = row['geo_point_2d']#.split(',')
-        print(f'zip: {zip_code} in Stadt: {zip_name_short} in kreis: {zip_kreis_name} with points: {zip_area_points}')
-
+        zip_area_points = row['geo_point_2d'].split(',')
+        print(f"""
+        INSERT INTO location_zip_code (id, zip_code, country, latitude, longitude, city)
+            SELECT gen_random_uuid(), {zip_code}, 'GER', {zip_area_points[0]}, {zip_area_points[1]}, '{zip_name_short}'
+        WHERE NOT EXISTS(
+            SELECT 1 FROM location_zip_code
+             WHERE country = 'GER'
+               AND zip_code = '{zip_code}'
+        );
+        """)
