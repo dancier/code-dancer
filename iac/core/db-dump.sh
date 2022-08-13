fd6e57223e3a39f7e33a2a1c1fbee3859b2c2103
Marc Gorzala <marc@dancier.net>
2022-08-13 10:07:13 +0200

    "logging"

--- iac/core/db-dump.sh
+++ iac/core/db-dump.sh
@@ -1,3 +1,5 @@
 NOW=$(date +"%Y-%m-%d-%H-%M")
-
-docker exec dancer-db pg_dump -h localhost -U dancer dancer > db_dump_${NOW} 
+FILENAME=db_dump_${NOW}.sql
+echo "Dumping into ${FILENAME}"
+docker exec dancer-db pg_dump -h localhost -U dancer dancer > $FILENAME
+echo "Done"
