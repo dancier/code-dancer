351868bd0b4fad6d6ffdb60a84c74dd5c4d2c69e
Marc Gorzala <marc@dancier.net>
2022-11-26 23:02:12 +0100

    typo

--- iac/core/update-dancier.sh
+++ iac/core/update-dancier.sh
@@ -2,10 +2,10 @@
 
 cd /run-env
 
-./db_dump_chat_dancer.sh
-./db_dump_dancer.sh
-./db_dump_iam.sh
-./db_dump_recommendation.sh
+./db-dump_chat_dancer.sh
+./db-dump_dancer.sh
+./db-dump_iam.sh
+./db-dump_recommendation.sh
 docker-compose pull
 docker-compose down
 docker-compose up --build -d
