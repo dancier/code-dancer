0b1d36519b2744a8167b98b5637cc8903310be93
Marc Gorzala <marc@dancier.net>
2022-11-26 22:50:36 +0100

    correct password

--- iac/core/db-dump_iam.sh
+++ iac/core/db-dump_iam.sh
@@ -2,5 +2,5 @@ cd /run-env
 NOW=$(date +"%Y-%m-%d-%H-%M-%S")
 FILENAME=db_dump_iam_${NOW}.sql
 echo "Dumping into ${FILENAME}"
-docker exec iam-db pg_dump -h localhost -U iam iam > $FILENAME
+docker exec iam-db pg_dump -h localhost -U keycloak keycloak > $FILENAME
 echo "Done"
