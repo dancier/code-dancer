a0e088c2edc49a4548d1a8ac220ff55603a7db5c
Marc Gorzala <marc@dancier.net>
2022-11-26 23:03:12 +0100

    typo

--- iac/core/db-dump_dancer.sh
+++ iac/core/db-dump_dancer.sh
@@ -2,5 +2,5 @@ cd /run-env
 NOW=$(date +"%Y-%m-%d-%H-%M-%S")
 FILENAME=db_dump_dancer_${NOW}.sql.gz
 echo "Dumping into ${FILENAME}"
-docker exec dancer-db pg_dump -h localhost -U dancer dancer| gzip -9 > $FILENAME
+docker exec dancer-db pg_dump -h localhost -U dancer dancer | gzip -9 > $FILENAME
 echo "Done"
