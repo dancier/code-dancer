4694e6cef4d5d24e74ab2dd693259e698348532e
Marc Gorzala <marc@dancier.net>
2022-11-26 22:42:01 +0100

    scripte to dump chat-dancer, reommendation

--- iac/core/db-dump.sh
+++
@@ -1,6 +0,0 @@
-cd /run-env
-NOW=$(date +"%Y-%m-%d-%H-%M-%S")
-FILENAME=db_dump_${NOW}.sql
-echo "Dumping into ${FILENAME}"
-docker exec dancer-db pg_dump -h localhost -U dancer dancer > $FILENAME
-echo "Done"
