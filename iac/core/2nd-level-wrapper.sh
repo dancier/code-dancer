b3923673d835be2074981a7b2b95dbfdef62b837
Marc Gorzala <marc@dancier.net>
2022-11-28 15:08:33 +0100

    debug

--- iac/core/2nd-level-wrapper.sh
+++ iac/core/2nd-level-wrapper.sh
@@ -14,6 +14,7 @@ function deploy() {
     VAR_NAME=${VAR_NAME//-/_}
     export ${VAR_NAME}=${2}
     echo "Using Tag var: ${VAR_NAME}"
+    ecoh "With Value: ${VAR_NAME}"
     echo "Pulling ${SERVICE} with TAG: $2"
     docker-compose pull ${SERVICE}
     docker-compose up -d --no-deps ${SERVICE}
