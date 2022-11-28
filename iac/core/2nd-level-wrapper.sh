5ef5ea4cfe3332a453809adb235e031844741c59
Marc Gorzala <marc@dancier.net>
2022-11-28 15:15:02 +0100

    bug

--- iac/core/2nd-level-wrapper.sh
+++ iac/core/2nd-level-wrapper.sh
@@ -12,9 +12,9 @@ function deploy() {
     export SERVICE=$1
     VAR_NAME=${SERVICE^^}_TAG
     VAR_NAME=${VAR_NAME//-/_}
-    export ${VAR_NAME}=${2}
+    export ${!VAR_NAME}=${2}
     echo "Using Tag var: ${VAR_NAME}"
-    echo "With Value: ${${VAR_NAME}}"
+    echo "With Value: ${!VAR_NAME}"
     echo "Pulling ${SERVICE} with TAG: $2"
     docker-compose pull ${SERVICE}
     docker-compose up -d --no-deps ${SERVICE}
