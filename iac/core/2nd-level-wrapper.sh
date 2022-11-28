060a6432bd66ac8fca58c367091af1c4cf3d4cd1
Marc Gorzala <marc@dancier.net>
2022-11-28 14:14:08 +0100

    version now selectable

--- iac/core/2nd-level-wrapper.sh
+++ iac/core/2nd-level-wrapper.sh
@@ -10,9 +10,8 @@ cd ${RUN_ENV}
 
 function deploy() {
     export SERVICE=$1
-    export ${SERVICE}_TAG=$2
+    export ${SERVICE^^}_TAG=$2
     echo "Pulling ${SERVICE} with TAG: $2"
-    echo $DANCER_TAG
     docker-compose pull ${SERVICE}
     docker-compose up -d --no-deps ${SERVICE}
 }
