5af55e6f1690a003c6a77c30b27f3c69fe3fe236
Marc Gorzala <marc@dancier.net>
2022-12-08 07:33:13 +0100

    recommendation-db to postgresql 15

--- iac/core/settings-template.sh
+++ iac/core/settings-template.sh
@@ -1,4 +1,16 @@
-# Service-Dancer
+###################################
+# Dancer DB
+###################################
+
+export DANCER_DB_HOSTNAME=dancer-dbexport DANCER_DB_HOSTNAME=dancer-db
+
+export DANCER_DB_USER=dancer
+export DANCER_DB_PASSWORD=secret
+export DANCER_DB_DB=dancer
+
+###################################
+# Dancer Service
+###################################
 
 export SPRING_DATASOURCE_URL=jdbc:postgresql://${DANCER_DB_HOSTNAME}:5432/${DANCER_DB_DB}
 export SPRING_DATASOURCE_USERNAME=${DANCER_DB_USER}
@@ -9,20 +21,25 @@ export SPRING_PROFILES_ACTIVE=staging,json-logging
 export APP_MAIL_USER=dev@dancier.net
 export APP_MAIL_PASS=whatever
 export APP_CAPTCHA_APIKEY=captcha-api-key
+export DANCER_TAG=latest
+export APP_CAPTCHA_MAGICTOKEN=secret
+export APP_ADMIN_EMAIL=marc@becheftigt.de
+export APP_ADMIN_PASS=secret
 
-
-# DB-Dancer
-
-export DANCER_DB_HOSTNAME=dancer-db
-export DANCER_DB_USER=dancer
-export DANCER_DB_PASSWORD=secret
-export DANCER_DB_DB=dancer
-
+###################################
 # PG Admin
+###################################
 
 export PG_ADMIN_PASSWORD=secret
+export PG_ADMIN_OAUTH2_SECRET=secret
+export PG_ADMIN_OAUTH2_TOKEN_URL=https://test-iam.dancier.net/realms/dancier/protocol/openid-connect/token
+export PG_ADMIN_OAUTH2_AUTHORIZATION_URL=https://test-iam.dancier.net/realms/dancier/protocol/openid-connect/auth
+export PG_ADMIN_OAUTH2_API_BASE_URL=https://test-iam.dancier.net/realms/dancier
+export PG_ADMIN_OAUTH2_USERINFO_ENDPOINT=https://test-iam.dancier.net/realms/dancier/protocol/openid-connect/userinfo
+export PG_ADMIN_HOSTNAME=pgadmin.dancier.net
 
 
+# Fluentd
 # for delivering the files
 export ELASTIC_HOST=elastic.dancier.net
 export ELASTIC_USER=elastic
@@ -30,11 +47,10 @@ export ELASTIC_PASS=secret
 
 export BACKEND_HOSTNAME=dancer.dancier.net
 
-export PG_ADMIN_HOSTNAME=pgadmin.dancier.net
 
 export SHOW_DANCER_HOSTNAME=dancier.net
 
-export DANCER_TAG=1.0
+# Traefik
 
 export TRAEFIK_TAG=traefik
 
@@ -46,55 +62,40 @@ export IAM_ADMIN_PASSWD=foobar123
 
 export IAM_HOST=iam.dancier.net
 
+
+# S3
+
 export S3_ADMIN_PASSWD=secret
-
 export S3_HOST=test-s3.dancier.net
-
 export S3_UI_HOST=test-s3-ui.dancier.net
-
 export S3_CLIENT_SECRET=secret
-
 export S3_OPENID_CONFIG_URL=https://test-iam.dancier.net/realms/dancier/.well-known/openid-configuration
 
-export PG_ADMIN_OAUTH2_SECRET=secret
-
-export PG_ADMIN_OAUTH2_TOKEN_URL=https://test-iam.dancier.net/realms/dancier/protocol/openid-connect/token
-
-export PG_ADMIN_OAUTH2_AUTHORIZATION_URL=https://test-iam.dancier.net/realms/dancier/protocol/openid-connect/auth
-
-export PG_ADMIN_OAUTH2_API_BASE_URL=https://test-iam.dancier.net/realms/dancier
-
-export PG_ADMIN_OAUTH2_USERINFO_ENDPOINT=https://test-iam.dancier.net/realms/dancier/protocol/openid-connect/userinfo
-
-export APP_CAPTCHA_MAGICTOKEN=secret
-
-export APP_ADMIN_EMAIL=marc@becheftigt.de
-
-export APP_ADMIN_PASS=secret
+# Recommendation
 
 export RECOMMENDATION_DB_USER=recommendation
-
 export RECOMMENDATION_DB_PASSWORD=secret
-
 export RECOMMENDATION_DB=recommendation
+export RECOMMENDATION_S3_SECRET=geheim
+export RECOMMENDATION_S3_TOKENURI=https://test-iam.dancier.net/realms/dancier/protocol/openid-connect/token
+export RECOMMENDATION_S3_STS_ENDPOINT=https://test-s3.dancier.net
+export RECOMMENDATION_S3_HOST=test-s3.dancier.net
+export RECOMMENDATION_DB_TAG=recommendation-db
+
+
+# Chat Dancer
 
 export CHAT_DANCER_DB_USER=chat-dancer
-
 export CHAT_DANCER_DB_PASSWORD=secret
-
 export CHAT_DANCER_DB=chat-dancer
+export CHAT_DANCER_TAG=1.0
+export CHAT_DANCER_DB=chat-dancer-db
+
 
 export DANCIER_TOKEN_URI=https://test-iam.dancier.net/realms/dancier/protocol/openid-connect/token
 
 export S3_OIDC_CLIENT_ID=s3
 
-export RECOMMENDATION_S3_SECRET=geheim
-
-export RECOMMENDATION_S3_TOKENURI=https://test-iam.dancier.net/realms/dancier/protocol/openid-connect/token
-
-export RECOMMENDATION_S3_STS_ENDPOINT=https://test-s3.dancier.net
-
-export RECOMMENDATION_S3_HOST=test-s3.dancier.net
 
 export KAFKA_UI_HOSTNAME=test-kafka-ui.dancier.net
 
@@ -104,11 +105,7 @@ export RECOMMENDATION_TAG=latest
 
 export DANCER_DB_TAG=dancer-db
 
-export CHAT_DANCER_TAG=1.0
 
-export CHAT_DANCER_DB=chat-dancer-db
-
-export RECOMMENDATION_DB_TAG=recommendation-db
 
 export IAM_TAG=iam
 
