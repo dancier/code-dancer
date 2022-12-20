bb7551306efd47ca38af9fb6b7d864b9ff85383f
Marc Gorzala <marc@dancier.net>
2022-12-20 16:59:26 +0100

    added settings template

---
+++ iac/efk/settings-template.sh
@@ -0,0 +1,7 @@
+export ELASTIC_VERSION=8.5.3
+export ELASTIC_PASSWORD=secrect
+export KIBANA_SYSTEM_PASSWORD=secrect
+
+
+export KIBANA_HOST_NAME=test-dancelake.dancier.net
+export ELASTIC_HOST_NAME=test-elastic.dancier.net
