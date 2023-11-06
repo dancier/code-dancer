e4ac6034363da9647f777c5af7f900cafa9927a7
Marc Gorzala <marc@dancier.net>
2023-11-06 07:48:11 +0100

    create chat endpoint fixed

--- dast/services/chat_dancer.py
+++ dast/services/chat_dancer.py
@@ -11,16 +11,17 @@ class RestClient:
     def __init__(self) -> None:
         pass
 
-    def create(self):
+    def create(self, participant_ids):
         headers = {
             "Content-Type": "application/json"
         }
 
         payload = {
-            "dancerIds": ["foo", "bar"]
+            "dancerIds": participant_ids
         }
         res = requests.post(self.hostname + "/h/chats", headers=headers, data=json.dumps(payload))
         return res
+
 
     def get_chats(self, dancer_id):
         headers = {
@@ -45,74 +46,3 @@ class RestClient:
 
         res = requests.post(self.hostname + "/chats", headers=headers, data=json.dumps(payload))
         return res
-
-    def event_log(self):
-        headers = {
-            "Content-Type": "application/json",
-        }
-
-        payload = {
-            "topic": "test123",
-            "metaData": {"hallo": "jan"},
-            "payload": {"engles": "schick doch auch mal was"}
-        }
-        r = requests.post(self.hostname + "/eventlog", headers=headers, data=json.dumps(payload))
-        return r
-
-    def auth_whoami(self):
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + self.cached_token
-        }
-        r = requests.get(self.hostname + "/authentication/whoami", headers=headers)
-        return r
-
-    def auth_login(self, email, password):
-        print("Login in with:" + email)
-        headers = {
-            "Content-Type": "application/json",
-        }
-        payload = {
-            "email": email,
-            "password": password
-        }
-        r = requests.post(self.hostname + "/authentication/login", headers=headers, data=json.dumps(payload))
-        if r.status_code == 200:
-            print("Login successful: ")
-            print("Token stored for further reference")
-            self.cached_token = r.json()["accessToken"]
-        return r
-
-    def auth_registrations(self, email, password):
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + self.cached_token
-        }
-        payload = {
-            "email": email,
-            "password": password,
-            "acceptTermsAndConditions": True
-        }
-        r = requests.post(self.hostname + "/authentication/registrations", headers=headers, data=json.dumps(payload))
-        return r
-
-    def profile_put(self, payload):
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + self.cached_token
-        }
-        print("Putting: " + str(payload))
-        r = requests.put(self.hostname + "/profile", headers=headers, data=json.dumps(payload))
-        return r
-
-    def auth_setValidationStatus(self, email, validated=True):
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + self.cached_token
-        }
-        payload = {
-            "emailAddress": email,
-            "validated": validated
-        }
-        r = requests.put(self.hostname + "/authentication/email-validations", headers=headers, data=json.dumps(payload))
-        return r
