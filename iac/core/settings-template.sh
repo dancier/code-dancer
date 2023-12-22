504d354736166365a960fadf4fbc583c34847c5c
Marc Gorzala <marc@dancier.net>
2023-12-22 09:59:27 +0100

    updated settings template

--- iac/core/settings-template.sh
+++ iac/core/settings-template.sh
@@ -26,6 +26,8 @@ export DANCER_TAG=latest
 export APP_CAPTCHA_MAGICTOKEN=secret
 export APP_ADMIN_EMAIL=marc@becheftigt.de
 export APP_ADMIN_PASS=secret
+export BACKEND_HOSTNAME=dancer.dancier.net
+export DANCIER_TOKEN_URI=https://test-iam.dancier.net/realms/dancier/protocol/openid-connect/token
 
 ################################
 # Chat Dancer DB
@@ -61,29 +63,30 @@ export PG_ADMIN_HOSTNAME=pgadmin.dancier.net
 ##################################
 # Fluentd
 ##################################
+
 export ELASTIC_HOST=elastic.dancier.net
 export ELASTIC_USER=elastic
 export ELASTIC_PASS=secret
 
-export BACKEND_HOSTNAME=dancer.dancier.net
 
+##################################
+# Show Dancer
+##################################
 
 export SHOW_DANCER_HOSTNAME=dancier.net
-
-# Traefik
-
-export TRAEFIK_TAG=traefik
-
 export SHOW_DANCER_TAG=latest
-
 export DANCIER_BACKEND_URL=https://dancer.dancier.net
 
-export IAM_ADMIN_PASSWD=foobar123
+##################################
+# IAM
+##################################
 
+export IAM_ADMIN_PASSWD=foobar123
 export IAM_HOST=iam.dancier.net
 
-
+####################################
 # S3
+####################################
 
 export S3_ADMIN_PASSWD=secret
 export S3_HOST=test-s3.dancier.net
@@ -91,42 +94,44 @@ export S3_UI_HOST=test-s3-ui.dancier.net
 export S3_CLIENT_SECRET=secret
 export S3_OPENID_CONFIG_URL=https://test-iam.dancier.net/realms/dancier/.well-known/openid-configuration
 
-# Recommendation
+####################################
+# Recommendation DB
+####################################
 
 export RECOMMENDATION_DB_USER=recommendation
 export RECOMMENDATION_DB_PASSWORD=secret
 export RECOMMENDATION_DB=recommendation
+
+####################################
+# Recommendation
+####################################
+
 export RECOMMENDATION_S3_SECRET=geheim
 export RECOMMENDATION_S3_TOKENURI=https://test-iam.dancier.net/realms/dancier/protocol/openid-connect/token
 export RECOMMENDATION_S3_STS_ENDPOINT=https://test-s3.dancier.net
 export RECOMMENDATION_S3_HOST=test-s3.dancier.net
-export RECOMMENDATION_DB_TAG=recommendation-db
+export RECOMMENDATION_TAG=latest
 
-
-
-export DANCIER_TOKEN_URI=https://test-iam.dancier.net/realms/dancier/protocol/openid-connect/token
-
-export S3_OIDC_CLIENT_ID=s3
-
+#####################################
+# Kafka UI
+#####################################
 
 export KAFKA_UI_HOSTNAME=test-kafka-ui.dancier.net
 
-export COMPOSE_HTTP_TIMEOUT=120
+#####################################
+# S3
+#####################################
 
-export RECOMMENDATION_TAG=latest
+export S3_OIDC_CLIENT_ID=s3
 
-export DANCER_DB_TAG=dancer-db
+##################################
+# Prometheus
+##################################
 
-
-
-export IAM_TAG=iam
-
-export IAM_DB_TAG=iam-db
-
-export S3_TAG=s3
+export PROMETHEUS_UI_HOST=test-prometheus.dancier.net
 
 export RUN_ENV=/run-env
 
 export KIKERIKI_TAG=1.0
 
-export PROMETHEUS_UI_HOST=test-prometheus.dancier.net
+
